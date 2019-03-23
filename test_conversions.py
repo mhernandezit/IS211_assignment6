196fe39e15ea4ac4f3d5eb2ffa65062fe9dc4f48
mhernandezit <mhernandezit@gmail.com>
2019-03-23 09:37:07 -0400

    Code tested and complete, pre-lint/docstrings

--- test_conversions.py
+++ test_conversions.py
@@ -1,46 +1,48 @@
-import conversions
 import unittest
+import conversions
+import conversions_refactored
+""" Test conversions unittest package """
 
 class TestConversions(unittest.TestCase):
+    """ A class to test out the conversion module - I'm using a dictionary to create the test cases """
+    testCases = [
+        {"celsius": 200.00, "fahrenheit": 392.00, "kelvin": 473.15},
+        {"celsius": 125.00, "fahrenheit": 257.00, "kelvin": 398.15},
+        {"celsius": 0.00, "fahrenheit": 32.00, "kelvin": 273.15},
+        {"celsius": 232.78, "fahrenheit": 451.00, "kelvin": 505.93},
+        {"celsius": -231.15, "fahrenheit": -384.07, "kelvin": 42}
+    ]
 
-	testCases = [
-		{"C": 300.00, "F": 572.00, "K": 573.15},
-		{"C": 300.00, "F": 572.00, "K": 573.15},
-		{"C": 300.00, "F": 572.00, "K": 573.15},
-		{"C": 300.00, "F": 572.00, "K": 573.15},
-		{"C": 300.00, "F": 572.00, "K": 573.15}
-	]
+    def testCelciusToKelvin(self):
+        """ Test from Celsius to Kelvin """
+        for test in self.testCases:
+            result = conversions.convertCelsiusToKelvin(test["celsius"])
+            self.assertEqual(test["kelvin"], result)
 
-	def testCelciusToKelvin(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["C"])
-			self.assertEqual(test["K"], result)
+    def testCelsiusToFahrenheit(self):
+        for test in self.testCases:
+            result = conversions.convertCelsiusToFahrenheit(test["celsius"])
+            self.assertEqual(test["fahrenheit"], result)
 
-	def testCelciusToFahrenheit(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["C"])
-			self.assertEqual(test["F"], result)
+    def testFahrenheitToCelsius(self):
+        for test in self.testCases:
+            result = conversions.convertFahrenheitToCelsius(test["fahrenheit"])
+            self.assertEqual(test["celsius"], result)
 
-	def testFahrenheitToCelcius(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["F"])
-			self.assertEqual(test["C"], result)
+    def testFahrenheitToKelvin(self):
+        for test in self.testCases:
+            result = conversions.convertFahrenheitToKelvin(test["fahrenheit"])
+            self.assertEqual(test["kelvin"], result)
 
-	def testFahrenheitToKelvin(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["F"])
-			self.assertEqual(test["K"], result)
+    def testKelvinToFahrenheit(self):
+        for test in self.testCases:
+            result = conversions.convertKelvinToFahrenheit(test["kelvin"])
+            self.assertEqual(test["fahrenheit"], result)
 
-	def testKelvinToFahrenheit(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["K"])
-			self.assertEqual(test["F"], result)
-
-	def testKelvinToCelsius(self):
-		for test in self.testCases:
-			result = conversions.convertCelsiustoKelvin(test["K"])
-			self.assertEqual(test["C"], result)
-
+    def testKelvinToCelsius(self):
+        for test in self.testCases:
+            result = conversions.convertKelvinToCelsius(test["kelvin"])
+            self.assertEqual(test["celsius"], result)
 
 class testConvert(unittest.TestCase):
     """This class defines the test  for the convert function in
@@ -50,14 +52,26 @@ class testConvert(unittest.TestCase):
     def testConvertTempurature(self):
         """convert() should give known temperature result with known input."""
         self.assertEqual(conversions_refactored.convert(
-            'celsius', 'kelvin', 300), 573.15)
+            'celsius', 'kelvin', 200), 473.15)
         self.assertEqual(conversions_refactored.convert(
-            'celsius', 'fahrenheit', 390), 734.00)
+            'celsius', 'fahrenheit', 125), 257.00)
         self.assertEqual(conversions_refactored.convert(
-            'fahrenheit', 'celsius', -238), -150.00)
+            'fahrenheit', 'celsius', 32), 0.00)
         self.assertEqual(conversions_refactored.convert(
-            'fahrenheit', 'kelvin', 734), 663.15)
+            'fahrenheit', 'kelvin', 451), 505.93)
         self.assertEqual(conversions_refactored.convert(
-            'kelvin', 'fahrenheit', 0), -459.67)
+            'kelvin', 'fahrenheit', 42), -384.07)
         self.assertEqual(conversions_refactored.convert(
-            'kelvin', 'celsius', 413.15), 140.00)
+            'kelvin', 'celsius', 200), -73.15)
+
+    def testMirrorTemperature(self):
+        self.assertEqual(conversions_refactored.convert(
+            'celsius', 'celsius', 300), 300.00)
+        self.assertEqual(conversions_refactored.convert(
+            'fahrenheit', 'fahrenheit', 35), 35.00)
+        self.assertEqual(conversions_refactored.convert(
+            'kelvin', 'kelvin', 272), 272.00)
+
+
+if __name__ == '__main__':
+    unittest.main()
